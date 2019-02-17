557b8e1a19f35d6f928c03f777062c50d3a08dce
Ian Torres <iantorres@outlook.com>
2019-02-16 22:29:38 -0300

    Incremental to Auth

--- tests/Feature/AuthTest.php
+++ tests/Feature/AuthTest.php
@@ -6,8 +6,28 @@ use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
+use App\User;
+
 class AuthTest extends TestCase
 {
+    use RefreshDatabase;
+    
+    public function test_register_route()
+    {
+        $user = factory(User::class)->make();
+        $password = substr(md5(mt_rand()), 0, 6);
+        $response = $this->post('/register', [
+            'name' => $user->name,
+            'email' => $user->email,
+            'password' => $password,
+            'password_confirmation' => $password,
+        ]);
+        $response->assertRedirect('home');
+        $this->assertDatabaseHas('users', [
+            'email' => $user->email,
+        ]);
+    }
+    
     /**
      * A basic feature test example.
      *
